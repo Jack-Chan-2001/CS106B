90ee8712d239564ba6d45ff811f04a1df66a4678
Jack Chan <chenjieke@zju.edu.cn>
2022-05-04 09:43:57 +0800

    Reoganized assignments directory structure

---
+++ assignments/02_Serafini/Ngrams/src/ngrams.cpp
@@ -0,0 +1,20 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    // TODO: Finish the program!
+
+
+    cout << "Exiting." << endl;
+    return 0;
+}
