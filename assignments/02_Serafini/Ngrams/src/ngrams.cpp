8001283d365977d1d4a60f3de5e450b9a8050772
Jack Chan <chenjieke@zju.edu.cn>
2022-05-07 08:51:17 +0800

    finished assignment2: sarafini

--- assignments/02_Serafini/Ngrams/src/ngrams.cpp
+++ assignments/02_Serafini/Ngrams/src/ngrams.cpp
@@ -5,15 +5,18 @@
 #include <string>
 #include "io/simpio.h"
 #include "util/strlib.h"
+#include "util/random.h"
 #include "collections/hashmap.h"
 #include "collections/vector.h"
-#include "collections/set.h"
 
 using namespace std;
 
 void welcome();
 void input(fstream& infile);
 int getN();
+HashMap<Vector<string>, Vector<string>> getNGramMap(const int& N, fstream& infile);
+int getRandomWordNumber();
+void generateRandomText(HashMap<Vector<string>, Vector<string>>& NGramMap, const int& N, const int& randomWordNumber);
 
 int main() {
     // TODO: Finish the program!
@@ -22,6 +25,15 @@ int main() {
     fstream infile;
     input(infile);
     int N = getN();
+    HashMap<Vector<string>, Vector<string>> NGramMap = getNGramMap(N, infile);
+
+    cout << "DEBUG: " << NGramMap << endl;
+
+    int randomWordNumber;
+    while (randomWordNumber = getRandomWordNumber()) {
+        generateRandomText(NGramMap, N, randomWordNumber);
+    }
+    
 
     cout << "Exiting." << endl;
     return 0;
@@ -66,4 +78,82 @@ int getN()
         }
         return N;
     }
+}
+
+HashMap<Vector<string>, Vector<string>> getNGramMap(const int& N, fstream& infile)
+{
+    HashMap<Vector<string>, Vector<string>> NGramMap;
+    Vector<string> window, suffix;
+    string word;
+    Vector<string> words;
+
+    // read sentences
+    while (infile >> word) {
+        words.add(word);
+    }
+    for (int i = 0; i < words.size(); i++) {
+        for (int j = i; j < i + N - 1; j++) {
+            if (j >= words.size()) {
+                // wrapping
+                window.add(words[j % words.size()]);
+            } else {
+                window.add(words[j]);
+            }
+        }
+        if (NGramMap.containsKey(window)) {
+            suffix = NGramMap[window];
+        } else {
+            suffix.clear();
+        }
+        if (i + N - 1 >= words.size()) {
+            suffix.add(words[(i + N - 1) % words.size()]);
+        } else {
+            suffix.add(words[i + N - 1]);
+        }
+        NGramMap[window] = suffix;
+        window.clear();
+    }
+    return NGramMap;
+}
+
+int getRandomWordNumber()
+{
+    string strRandomWord;
+    int randomWordNumber;
+    while (true) {
+        strRandomWord = getLine("# of random words to generate (0 to quit)?");
+        if (!stringIsInteger(strRandomWord)) {
+            cout << "Illegal integer format. Try again.\n";
+            continue;
+        }
+        randomWordNumber = stringToInteger(strRandomWord);
+        if (randomWordNumber != 0 && randomWordNumber < 4) {
+            cout << "Must be at least 4 words.\n";
+            continue;
+        }         
+        return randomWordNumber;
+    }
+}
+
+void generateRandomText(HashMap<Vector<string>, Vector<string>>& NGramMap, const int& N, const int& randomWordNumber)
+{
+    cout << "... ";
+
+    // set up window
+    Vector<string> randomText = randomKey(NGramMap);
+    Vector<string> randomWindow = randomText;
+    string randomTextSuffix = randomElement(NGramMap[randomText]);
+
+    for (int i = 0; i < randomWordNumber - randomWindow.size(); i++) {
+        randomWindow.remove(0);
+        randomWindow.add(randomTextSuffix);
+        randomTextSuffix = randomElement(NGramMap[randomWindow]);
+        randomText.add(randomTextSuffix);
+    }
+
+    for (string word : randomText) {
+        cout << word << " ";
+    }
+
+    cout << "... \n";
 }
